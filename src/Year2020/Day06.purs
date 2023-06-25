2a446de25645085c4f80617fa5e41f3dabfe38d6
Konsta Purtsi <konsta@arado.fi>
2023-06-25 10:35:18 +0300

    Day 6 part 2

--- src/Year2020/Day06.purs
+++ src/Year2020/Day06.purs
@@ -1,13 +1,14 @@
 module Year2020.Day06 where
 
 import Prelude
-import Data.Array (nub, length)
+import Data.Array (nub, length, filter, all, elem, head)
 import Data.Foldable (foldl)
 import Data.String (split, replaceAll)
 import Data.String.CodeUnits (toCharArray)
 import Data.String.Pattern (Pattern(..), Replacement(..))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Class.Console (log, logShow)
 
 test :: String
 test =
@@ -27,6 +28,7 @@ a
 
 b"""
 
+--------------------------------------------------------------------------------
 parseGroup :: String -> Int
 parseGroup str =
   replaceAll (Pattern "\n") (Replacement "") str
@@ -34,7 +36,6 @@ parseGroup str =
     # nub
     # length
 
---------------------------------------------------------------------------------
 part1 :: String -> Effect Unit
 part1 input = do
   let
@@ -44,8 +45,27 @@ part1 input = do
   log $ "Part 1 ==> " <> show result
 
 --------------------------------------------------------------------------------
+parseGroup2 :: String -> Int
+parseGroup2 str =
+  let
+    persons =
+      split (Pattern "\n") str
+        # filter (_ /= "")
+        # map toCharArray
+
+    firstPerson = head persons
+  in
+    case firstPerson of
+      -- first person answers something -> check everyone answered it as well
+      Just fp ->
+        filter (\q -> all (\arr -> elem q arr) persons) fp
+          # length
+      Nothing -> 0
+
 part2 :: String -> Effect Unit
 part2 input = do
   let
-    result = "<TODO>"
-  log $ "Part 2 ==> " <> result
+    result =
+      split (Pattern "\n\n") input
+        # foldl (\acc g -> acc + parseGroup2 g) 0
+  log $ "Part 2 ==> " <> show result
