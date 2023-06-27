c8e27baffeb50f39c89928c6c66ea240839588ef
Konsta Purtsi <konsta@arado.fi>
2023-06-28 00:53:42 +0300

    Day 10 part 2

--- src/Year2020/Day10.purs
+++ src/Year2020/Day10.purs
@@ -1,13 +1,18 @@
 module Year2020.Day10 where
 
 import Prelude
-import Data.Array (mapMaybe, sort)
-import Data.Foldable (foldl)
+import Data.Array (mapMaybe, sort, (!!), (:), last, take, filter, length, head)
+import Data.BigInt (BigInt, fromInt, toString)
+import Data.Either (Either(..))
+import Data.Foldable (foldl, foldr)
 import Data.Int (fromString)
+import Data.Maybe (Maybe(..))
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
 import Effect.Class.Console (log, logShow)
+import Effect.Exception.Unsafe (unsafeThrow)
+import Debug (spy)
 
 example :: String
 example =
@@ -50,8 +55,51 @@ part1 input = do
   log $ "Part 1 ==> " <> show result
 
 --------------------------------------------------------------------------------
+
+-- the 1st example is a bait
+-- -> you can't always have two consecutive adapters missing
+-- 1 3 5 6 7 10 11 12 15 16 19 (22)
+-- 1 3 5 6 7 10 12 15 16 19 (22)
+-- 1 3 5 7 10 11 12 15 16 19 (22)
+-- 1 3 5 7 10 12 15 16 19 (22)
+-- 1 3 6 7 10 11 12 15 16 19 (22)
+-- 1 3 6 7 10 12 15 16 19 (22)
+
+-- 1 3 7 10 11 12 15 16 19 (22) (illegal)
+-- 1 3 7 10 12 15 16 19 (22) (illegal)
+
+-- dynamic programming -> collect tail in acc and do shit
+findArrangements :: Array Int -> Maybe { options :: BigInt, val :: Int }
+findArrangements adapters =
+  foldr
+    ( \val acc ->
+        let
+          threePrevious = take 3 acc
+          options =
+            if threePrevious == [] then fromInt 1
+            else foldl
+              ( \acc n ->
+                  if n.val - val <= 3 then acc + n.options else acc
+              )
+              (fromInt 0)
+              threePrevious
+        in
+          [ { val, options } ] <> acc
+    )
+    []
+    adapters
+    # head
+
 part2 :: String -> Effect Unit
 part2 input = do
   let
-    result = "<TODO>"
-  log $ "Part 2 ==> " <> result
+    adapters = parse input # sort
+    end = case last adapters of
+      Just l -> l + 3
+      Nothing -> unsafeThrow "huh"
+
+    withEnds = 0 : adapters <> [ end ]
+    result = case findArrangements withEnds of
+      Just r -> r.options
+      Nothing -> unsafeThrow "huh"
+  log $ "Part 2 ==> " <> toString result
