d4ce311a183b59fb12af0ade95c00ddd377c167d
Konsta Purtsi <konsta@arado.fi>
2023-07-03 21:07:01 +0300

    Day 13 part 2

--- spago.dhall
+++ spago.dhall
@@ -22,6 +22,7 @@ to generate this file without the comments in this block.
   , "exceptions"
   , "foldable-traversable"
   , "integers"
+  , "lists"
   , "maybe"
   , "node-buffer"
   , "node-fs"
--- src/Year2020/Day13.purs
+++ src/Year2020/Day13.purs
@@ -1,22 +1,30 @@
 module Year2020.Day13 where
 
 import Prelude
-import Data.Array (unsafeIndex, catMaybes)
+import Data.Array (catMaybes, reverse, snoc, sortWith, unsafeIndex)
+import Data.BigInt (BigInt, fromInt, toString)
 import Data.Foldable (foldl)
+import Data.FoldableWithIndex (foldlWithIndex)
 import Data.Int (fromString)
-import Data.Maybe (fromJust)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Data.Tuple (Tuple(..), snd)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Class.Console (log, logShow)
+import Effect.Class.Console (log)
 import Partial.Unsafe (unsafePartial)
 
 example :: String
 example =
   """939
 7,13,x,x,59,x,31,19
+"""
+
+example2 :: String
+example2 =
+  """
+1789,37,47,1889
 """
 
 parse :: String -> Tuple Int (Array Int)
@@ -32,6 +40,20 @@ parse str =
       # catMaybes
   in
     Tuple earliest buses
+
+parse2 :: String -> Array (Tuple BigInt BigInt)
+parse2 str =
+  unsafePartial $ unsafeIndex (split (Pattern "\n") str) 1
+    # split (Pattern ",")
+    # foldlWithIndex
+        ( \idx acc b -> case fromString b of
+            Just n -> snoc acc
+              $ Tuple
+                  (fromInt (idx `mod` n))
+                  (fromInt n)
+            Nothing -> acc
+        )
+        []
 
 --------------------------------------------------------------------------------
 findDeparture :: Int -> Int -> Int
@@ -57,8 +79,30 @@ part1 input = do
 
 --------------------------------------------------------------------------------
 
+findSubsequentDepartures :: Array (Tuple BigInt BigInt) -> BigInt
+findSubsequentDepartures buses =
+  let
+    sorted = sortWith snd buses # reverse
+  in
+    foldl
+      ( \{ start, step } (idx /\ id) ->
+          { start: findShit start step id idx
+          , step: id * step
+          }
+      )
+      { start: fromInt 0, step: fromInt 1 }
+      sorted
+      # _.start
+
+findShit :: BigInt -> BigInt -> BigInt -> BigInt -> BigInt
+findShit start step id idx =
+  if start `mod` id == (if idx == fromInt 0 then idx else id - idx) then start
+  else findShit (start + step) step id idx
+
 part2 :: String -> Effect Unit
 part2 input = do
-  let result = "<TODO>"
-  log $ "Part 2 ==> " <> result
+  let
+    buses = parse2 input
+    result = findSubsequentDepartures buses
+  log $ "Part 2 ==> " <> toString result
 
