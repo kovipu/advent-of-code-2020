fdcf1938881a47e398c88cd02c8d5a9fcd6cd272
Konsta Purtsi <konsta@arado.fi>
2023-07-11 13:31:01 +0300

    Day 18 part 1

---
+++ src/Year2020/Day18.purs
@@ -0,0 +1,141 @@
+module Year2020.Day18 where
+
+import Prelude
+
+import Control.Alternative (empty)
+import Control.Lazy (fix)
+import Data.Array (length, reverse)
+import Data.BigInt (BigInt, fromInt, toString)
+import Data.Foldable (foldl)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+import Data.Int (fromString)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Data.Tuple.Nested ((/\))
+import Data.String (split, joinWith, replaceAll, Replacement(..))
+import Data.String.CodeUnits (toCharArray, fromCharArray)
+import Data.String.Pattern (Pattern(..))
+import Effect (Effect)
+import Effect.Class.Console (log, logShow)
+import Effect.Exception (throw)
+import Parsing (Parser, runParser)
+import Parsing.Combinators ((<|>), option, optional)
+import Parsing.Combinators.Array (many)
+import Parsing.String (anyTill, string, char, anyChar)
+import Parsing.String.Basic (intDecimal)
+import Debug (spy)
+
+example :: String
+example =
+  """1 + 2 * 3 + 4 * 5 + 6
+"""
+
+example2 :: String
+example2 =
+  """1 + (2 * 3) + (4 * (5 + 6))
+"""
+
+data Expr
+  = Add Expr Expr
+  | Mul Expr Expr
+  | Lit Int
+
+instance Show Expr where
+  show (Add e1 e2) = "(" <> show e1 <> " + " <> show e2 <> ")"
+  show (Mul e1 e2) = "(" <> show e1 <> " * " <> show e2 <> ")"
+  show (Lit l) = show l
+
+-- order of operations was reversed -> smooth brain solution
+parse :: String -> Effect (Array Expr)
+parse input =
+  let
+    reversed = split (Pattern "\n") input
+      # map
+          ( toCharArray
+              >>> reverse
+              >>>
+                ( map \c -> case c of
+                    ')' -> '('
+                    '(' -> ')'
+                    other -> other
+                )
+              >>> fromCharArray
+          )
+      # joinWith "\n"
+  in
+    case runParser reversed (many expr) of
+      Right r -> pure r
+      Left _ -> throw "parsing failed"
+
+data Op = Plus | Mult
+
+expr :: Parser String Expr
+expr =
+  let
+    start :: Parser String Expr
+    start = group <|> lit
+
+    group :: Parser String Expr
+    group = do
+      _ <- char '('
+      e <- expr
+      _ <- char ')'
+      pure e
+
+    lit :: Parser String Expr
+    lit = do
+      d <- intDecimal
+      pure $ Lit d
+
+    end :: Parser String (Maybe (Tuple Op Expr))
+    end =
+      let
+        getEnd = do
+          _ <- char ' '
+          op <- char '+' <|> char '*'
+          _ <- char ' '
+          e <- expr
+          case op of
+            '+' -> pure $ Just $ Plus /\ e
+            '*' -> pure $ Just $ Mult /\ e
+            _ -> empty
+      in
+        getEnd <|> pure Nothing
+  in
+    do
+      e1 <- start
+      mE2 <- end
+      _ <- optional $ char '\n'
+      case mE2 of
+        Nothing -> pure e1
+        Just (Plus /\ e2) -> pure $ Add e1 e2
+        Just (Mult /\ e2) -> pure $ Mul e1 e2
+
+--------------------------------------------------------------------------------
+
+evaluate :: Expr -> BigInt
+evaluate (Add e1 e2) = evaluate e1 + evaluate e2
+evaluate (Mul e1 e2) = evaluate e1 * evaluate e2
+evaluate (Lit l) = (fromInt l)
+
+part1 :: String -> Effect Unit
+part1 input = do
+  expressions <- parse input
+  _ <- logShow expressions
+  let
+    result :: BigInt
+    result = foldl
+      (\acc e -> acc + (evaluate e))
+      (fromInt 0)
+      expressions
+  log $ "Part 1 ==> " <> toString result
+
+--------------------------------------------------------------------------------
+
+part2 :: String -> Effect Unit
+part2 input = do
+  let result = "<TODO>"
+  log $ "Part 2 ==> " <> result
+
